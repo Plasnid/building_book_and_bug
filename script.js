e7c59ccf2bb69ec236bbb0bc48c9041f2eab510e
Plasnid <stuart@sackler.ca>
2023-01-18 13:44:02 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/**
+ ** our symbols
+ *! -books
+ *! -lock
+ *! -Tower
+ *! -fountain
+ *! -bug
+ *! -speech bubble
+ *! -parchute
+ *! -radio
+ *! -cane
+ *
+ * *  parachuting into a locked tower of books
+ * * old person walking through a garden with a fountain
+ * * people playing in the park
+ * * talking bug on the fountain
+ * 
+ * * 3 title options
+ * * First working title: The building, the book and the bug
+ * 
+ * * The guardian of wisdom
+ * * Lock Books
+ * * The adrenaline starts here
+ */
